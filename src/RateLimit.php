40efe644d02ffea7b7ea58035cf05fcd43a4aa90
Francisco Jr <franciscojr.dev@gmail.com>
2023-12-27 16:53:05 -0300

    Changes

--- src/RateLimit.php
+++ src/RateLimit.php
@@ -6,41 +6,59 @@ namespace FjrSoftware\Flinkbot\Exchange;
 
 class RateLimit
 {
+    /**
+     * @var int
+     */
     private int $currentRequest = 0;
+
+    /**
+     * @var int
+     */
     private int $currentOrder = 0;
 
+    /**
+     * Set current request
+     *
+     * @param int $current
+     * @return RateLimit
+     */
     public function setCurrentRequest(int $current): RateLimit
     {
-        $this->currentRequest = $this->using($current, $this->currentRequest);
+        $this->currentRequest = $current;
 
         return $this;
     }
 
+    /**
+     * Get current request
+     *
+     * @return int
+     */
     public function getCurrentRequest(): int
     {
         return $this->currentRequest;
     }
 
+    /**
+     * Set current order
+     *
+     * @param int $current
+     * @return RateLimit
+     */
     public function setCurrentOrder(int $current): RateLimit
     {
-        $this->currentOrder = $this->using($current, $this->currentOrder);
+        $this->currentOrder = $current;
 
         return $this;
     }
 
+    /**
+     * Get current order
+     *
+     * @return int
+     */
     public function getCurrentOrder(): int
     {
         return $this->currentOrder;
-    }
-
-    private function using(int $current, int $previous): int
-    {
-        $using = 0;
-
-        if ($current) {
-            $using = $current - $previous;
-        }
-
-        return $using;
     }
 }
